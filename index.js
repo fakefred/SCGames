940c5c1f4f38b6610939babd1d108e3a01f1007d
fakefred <fakefred6@gmail.com>
2019-03-31 22:21:33 +0800

    app setup: backend, ui

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+app.use(express.static(__dirname + '/static'));
+
+const io = require('socket.io')(http);
+
+const config = require('./config.json');
+
+let players = new Set();
+let grid = [];
+
+function setToArray(set) {
+    let array = [];
+    set.forEach(item => {
+        array.push(item);
+    });
+    return array;
+}
+
+for (let y = 0; y < 8; y++) {
+    grid[y] = [];
+    for (let x = 0; x < 8; x++) {
+        grid[y][x] = '';
+    }
+}
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/static/grid.html');
+});
+
+io.on('connection', socket => {
+    let player = '';
+
+    socket.on('join', data => {
+        if (players.size < config['max-players']) {
+            if (!players.has(data.name)) {
+                player = data.name;
+                players.add(player);
+                socket.emit('identified', { name: player });
+                socket.broadcast.emit('player-list', {
+                    players: setToArray(players)
+                });
+                socket.emit('init', {
+                    players: setToArray(players),
+                    grid
+                });
+            } else {
+                socket.emit('err', { msg: 'This user name already exists' });
+            }
+        } else {
+            socket.emit('err', { msg: 'This room is full' });
+        }
+    });
+
+    socket.on('setchar', data => {
+        if (typeof data.y === 'number' && typeof data.x === 'number') {
+            console.log(data);
+            grid[data.y][data.x] = data.char;
+            socket.broadcast.emit('setchar', data);
+        }
+    });
+
+    socket.on('disconnect', () => {
+        if (players.has(player)) {
+            players.delete(player);
+        }
+        socket.broadcast.emit('player-list', { players: setToArray(players) });
+    });
+});
+
+http.listen(1937);
---
+++ static/script/grid.js
@@ -0,0 +1,95 @@
+const gridElem = document.querySelector('#grid');
+const gridContainer = document.querySelector('#grid-container');
+const screenWidth = window.innerWidth;
+gridContainer.style += ` left: ${(window.innerWidth - 368) / 2};`;
+
+function resetGrid() {
+    let html = '';
+    for (let y = 0; y < 8; y++) {
+        html += '<tr class="row">';
+        for (let x = 0; x < 8; x++) {
+            html += `<td><button class="btn btn-warning block" onclick="editBlock(${y}, ${x})"></button></td>`;
+        }
+        html += '</tr>';
+    }
+    gridElem.innerHTML = html;
+}
+
+resetGrid();
+
+function setGrid(grid) {
+    const flatGrid = grid.flat();
+    for (let n = 0; n < flatGrid.length; n++) {
+        document.getElementsByClassName('block')[n].innerHTML = flatGrid[n];
+    }
+}
+
+const socket = io('/');
+
+socket.on('err', data => {
+    alert(data.msg);
+});
+
+let name = 'user';
+
+function submitName() {
+    name = document.querySelector('#player-id').value;
+    socket.emit('join', { name });
+}
+
+socket.on('identified', data => {
+    document.querySelector('#welcome').hidden = true;
+});
+
+function updatePlayerList(players) {
+    let html = '';
+    players.forEach(player => {
+        html += `<li id="player-${player}" class="list-group-item">${player}</li>`;
+    });
+    document.querySelector('#players').innerHTML = html;
+}
+
+socket.on('init', data => {
+    updatePlayerList(data.players);
+    setGrid(data.grid);
+});
+
+socket.on('player-list', data => {
+    updatePlayerList(data.players);
+});
+
+let editing = {};
+
+function editBlock(y, x) {
+    editing = { y, x };
+    document.querySelector(
+        '#char-input'
+    ).value = document.getElementsByClassName('block')[8 * y + x].innerHTML;
+}
+
+function setBlock(y, x, char) {
+    document.getElementsByClassName('block')[8 * y + x].innerHTML = char;
+}
+
+function submitChar() {
+    const char = document.querySelector('#char-input').value;
+    if (char !== '' && editing !== {}) {
+        socket.emit('setchar', {
+            y: editing.y,
+            x: editing.x,
+            char,
+            player: name
+        });
+        setBlock(editing.y, editing.x, char);
+        editing = {};
+    }
+}
+
+socket.on('setchar', data => {
+    setBlock(data.y, data.x, data.char);
+    const playerElem = document.querySelector(`#player-${data.player}`);
+    playerElem.classList.add('active');
+    setTimeout(() => {
+        playerElem.classList.remove('active');
+    }, 200);
+});
